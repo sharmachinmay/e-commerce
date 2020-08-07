f987a76f7a87d357b2bf8768cb7be21b42ee496c
unknown <sharmaceaser5678@gmail.com>
2020-08-07 21:35:50 +0530

    change to withRouter

--- src/components/menu-items/menu-items.component.jsx
+++ src/components/menu-items/menu-items.component.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import withRouter from 'react-router-dom';
+import { withRouter } from 'react-router-dom';
 import './menu-items.styles.scss';
 
 const MenuItem = (props) => (
